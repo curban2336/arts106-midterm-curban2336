86920e6db060efac975eef57b52c0854c8a32f05
Curban841 <curban841@gmail.com>
2020-12-04 22:57:33 -0800

    Added Generator particle effects and triggers for particles

---
+++ Assets/DoubleActivation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleActivation : MonoBehaviour
+{
+    public Transform particle;
+    public Transform duo;
+    public Transform generator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        generator.GetComponent<ParticleSystem>().enableEmission = false;
+
+        if (particle.GetComponent<ParticleSystem>().enableEmission == true)
+        {
+            if (duo.GetComponent<ParticleSystem>().enableEmission == true)
+            {
+                generator.GetComponent<ParticleSystem>().enableEmission = true;
+            }
+        }
+    }
+}
---
+++ Assets/ParticleEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEvent : MonoBehaviour
+{
+    public Transform particle;
+    public Transform generator;
+    public Transform duo;
+
+    void Start()
+    {
+        particle.GetComponent<ParticleSystem>().enableEmission = false;
+        generator.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            particle.GetComponent<ParticleSystem> ().enableEmission = true;
+
+        }
+    }
+
+    void Update()
+    {
+        if (particle.GetComponent<ParticleSystem>().enableEmission == true)
+        {
+            if (duo.GetComponent<ParticleSystem>().enableEmission == true)
+            {
+                generator.GetComponent<ParticleSystem>().enableEmission = true;
+            }
+        }
+    }
+
+}
