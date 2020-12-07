a97b5aca58243095fdf8e5b11befdbdd907c1b2e
Curban841 <curban841@gmail.com>
2020-12-06 17:05:54 -0800

    Alternate composition scene

---
+++ Assets/UIMANAGER.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMANAGER : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MidtermProject");
+    }
+
+    public void Composition()
+    {
+        SceneManager.LoadScene("Composition");
+    }
+}
